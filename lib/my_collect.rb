4587abc00ad9c7340f30f13b6e3a203ecb64e9cd
Ian Vlahovic <banddorkfht7@gmail.com>
2018-01-14 01:17:24 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  empty_array = []
+  i = 0
+  if block_given?
+    while i < array.length
+      empty_array << yield(array[i])
+      i += 1
+    end
+  end
+  empty_array
+end
